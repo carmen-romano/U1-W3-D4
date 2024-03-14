4383076f04e1a1beaa2ef2a56d7a5cffc2ecc386
Carmen <my-9@hotmail.it>
2024-03-14 20:48:12 +0100

    Aggiunte funzionalità mancanti

--- assets/tombola.js
+++ assets/tombola.js
@@ -11,8 +11,12 @@ const generateTable = () => {
   }
 };
 
+const random = function () {
+  let randomNumber = Math.floor(Math.random() * 91);
+  return randomNumber;
+};
 button.addEventListener("click", () => {
-  let randomNumber = Math.floor(Math.random() * 90) + 1;
+  let randomNumber = random();
   let randomDiv = document.createElement("div");
   randomDiv.className = "numeriEstratti";
   let randomP = document.createElement("p");
@@ -26,8 +30,36 @@ button.addEventListener("click", () => {
       number.classList.add("numeroEstratto");
     }
   });
+
+  let cartellaGiocatore = document.querySelectorAll(".numeroGiocatore");
+  cartellaGiocatore.forEach((cartella) => {
+    if (parseInt(cartella.innerText) === randomNumber) {
+      cartella.classList.add("numeroEstratto");
+    }
+  });
 });
 
+let containerCartelle = document.getElementById("containerCartelle");
+let numeroCartelle = document.getElementById("numberInput");
+btnNumeroCartelle.addEventListener("click", (e) => {
+  containerCartelle.innerHTML = "";
+
+  for (let i = 0; i < numeroCartelle.value; i++) {
+    let divCartella = document.createElement("div");
+    let cartellaH1 = document.createElement("h3");
+    cartellaH1.innerText = "La tua cartella";
+    divCartella.appendChild(cartellaH1);
+    divCartella.className = "divCartella";
+    for (let j = 0; j < 24; j++) {
+      let randomNumber = random();
+      let cartellaGiocatore = document.createElement("p");
+      cartellaGiocatore.innerText = randomNumber;
+      cartellaGiocatore.className = "numeroGiocatore";
+      divCartella.appendChild(cartellaGiocatore);
+      containerCartelle.appendChild(divCartella);
+    }
+  }
+});
 window.onload = () => {
   generateTable();
 };
